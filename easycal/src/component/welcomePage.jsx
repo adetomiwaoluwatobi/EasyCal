19127694db86e4228d9fc9ceaed265641ab14353
teetwothefirst <teetwothefirst@gmail.com>
2024-08-30 11:50:37 +0000

    Changes to Welcome.JSC

--- easycal/src/component/welcomePage.jsx
+++ easycal/src/component/welcomePage.jsx
@@ -1,8 +1,8 @@
 function welcomePage(){
     return(
         <>
-            <div>
-                <header>
+            <div className="p-2">
+                <header className="headerStyle p-3">
                     <div class="px-3">
                         <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect width="30" height="6" rx="3" fill="#B4BDD8"/>
@@ -14,10 +14,19 @@ function welcomePage(){
                     </div>
                     <div>
                         <button>Login</button>
-                        <button class="px-2 py-1">Get Started</button>
+                        <button class="px-2 py-1 border rounded bg-secondary mx-2">Get Started</button>
                     </div>
                 </header>
-                <main></main>
+                <main className="mt-5">
+                    <div className="flex flex-row">
+                        <div>
+                            <h3>Unlock Smarter Loan Decisions</h3>
+                            <p>Get clear insights on your payments, costs, and financial health. Start making smarter loan decisions today.</p>
+                            <button className="bg-blue-950 text-white border rounded-full p-2"> Get Started <i className="bi bi-arrow-"></i></button>
+                        </div>
+                        <div><img src="../assets/landingPageAsset1.png" alt="CalculatorPNG"/></div>
+                    </div>
+                </main>
                 <footer></footer>
             </div>
         </>
