9b41fcba84b1a18014bf7c2e9b613d839de13e15
adetomiwaoluwatobi <teetwothefirst@gmail.com>
2024-09-01 00:30:01 +0000

    Push little update

--- easycal/src/main.jsx
+++ easycal/src/main.jsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
